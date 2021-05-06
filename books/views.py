64fda33358b15490115d0f7211ac1c1dd4f4f41d
Joe Sylnice <nerdroid23@gmail.com>
2021-05-06 13:27:37 -0400

    books index page
    https://youtu.be/aY43fUGlB7E?t=3736

--- books/views.py
+++ books/views.py
@@ -1,3 +1,15 @@
+import json
+from pathlib import Path
+
 from django.shortcuts import render
 
-# Create your views here.
+booksReader = open(Path(__file__).parent.joinpath('data/books.json'))
+data = json.loads(booksReader.read())
+booksReader.close()
+
+
+def index(request):
+    return render(request, 'books/index.html', {
+        'name': 'John',
+        'books': data[:10]
+    })
